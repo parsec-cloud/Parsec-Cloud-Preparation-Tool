1a0664d2748e5f969bb9a2e81a2abed6f5e5a56a
James Stringer <38541878+jamesstringerparsec@users.noreply.github.com>
2019-09-03 17:09:43 +0200

    Update PostInstall.ps1

--- PostInstall/PostInstall.ps1
+++ PostInstall/PostInstall.ps1
@@ -420,21 +420,21 @@ Start-Sleep 1
 Function InstallViGEmBus {
 #Required for Controller Support.
 Write-Host "Installing ViGEmBus - https://github.com/ViGEm/ViGEmBus"
-$Vigem = @{}
-$Vigem.DriverFile = "C:\Program Files\Parsec\Vigem\ViGEmBus.cat";
-$Vigem.CertName = 'C:\Program Files\Parsec\Vigem\Wohlfeil_IT_e_U_.cer';
-$Vigem.ExportType = [System.Security.Cryptography.X509Certificates.X509ContentType]::Cert;
-$Vigem.Cert = (Get-AuthenticodeSignature -filepath $vigem.DriverFile).SignerCertificate; 
-$Vigem.CertInstalled = if ((Get-ChildItem -Path Cert:\CurrentUser\TrustedPublisher | Where-Object Subject -Like "*CN=Wohlfeil.IT e.U., O=Wohlfeil.IT e.U.*" ) -ne $null) {$True}
-Else {$false}
-if ($vigem.CertInstalled -eq $true) {
-cmd.exe /c '"C:\Program Files\Parsec\vigem\devcon.exe" install "C:\Program Files\Parsec\vigem\ViGEmBus.inf" Root\ViGEmBus' | Out-Null
-} 
-Else {[System.IO.File]::WriteAllBytes($Vigem.CertName, $Vigem.Cert.Export($Vigem.ExportType));
-Import-Certificate -CertStoreLocation Cert:\LocalMachine\TrustedPublisher -FilePath 'C:\Program Files\Parsec\Vigem\Wohlfeil_IT_e_U_.cer' | Out-Null
-Start-Sleep 5
-cmd.exe /c '"C:\Program Files\Parsec\vigem\devcon.exe" install "C:\Program Files\Parsec\vigem\ViGEmBus.inf" Root\ViGEmBus' | Out-Null
-}
+#$Vigem = @{}
+#$Vigem.DriverFile = "C:\Program Files\Parsec\Vigem\ViGEmBus.cat";
+#$Vigem.CertName = 'C:\Program Files\Parsec\Vigem\Wohlfeil_IT_e_U_.cer';
+#$Vigem.ExportType = [System.Security.Cryptography.X509Certificates.X509ContentType]::Cert;
+#$Vigem.Cert = (Get-AuthenticodeSignature -filepath $vigem.DriverFile).SignerCertificate; 
+#$Vigem.CertInstalled = if ((Get-ChildItem -Path Cert:\CurrentUser\TrustedPublisher | Where-Object Subject -Like "*CN=Wohlfeil.IT e.U., O=Wohlfeil.IT e.U.*" ) -ne $null) {$True}
+#Else {$false}
+#if ($vigem.CertInstalled -eq $true) {
+cmd.exe /c '"C:\Program Files\Parsec\vigem\10\x64\devcon.exe" install "C:\Program Files\Parsec\vigem\10\ViGEmBus.inf" Nefarius\ViGEmBus\Gen1' | Out-Null
+#} 
+#Else {[System.IO.File]::WriteAllBytes($Vigem.CertName, $Vigem.Cert.Export($Vigem.ExportType));
+#Import-Certificate -CertStoreLocation Cert:\LocalMachine\TrustedPublisher -FilePath 'C:\Program Files\Parsec\Vigem\Wohlfeil_IT_e_U_.cer' | Out-Null
+#Start-Sleep 5
+#cmd.exe /c '"C:\Program Files\Parsec\vigem\devcon.exe" install "C:\Program Files\Parsec\vigem\ViGEmBus.inf" Root\ViGEmBus' | Out-Null
+#}
 }
 
 Function CreateFireWallRule {
