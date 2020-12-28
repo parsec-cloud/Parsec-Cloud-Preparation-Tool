6597bbff45d6ef2cac4eed5455b57cfcecf1e56f
James Stringer <38541878+jamesstringerparsec@users.noreply.github.com>
2020-12-28 12:59:18 +0100

    Updated TLS config for Powershell downloads

--- PostInstall/PostInstall.ps1
+++ PostInstall/PostInstall.ps1
@@ -1,6 +1,8 @@
 param (
     [switch]$DontPromptPasswordUpdateGPU
     )
+    
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
 
 $host.ui.RawUI.WindowTitle = "Parsec Cloud Preparation Tool"
 
